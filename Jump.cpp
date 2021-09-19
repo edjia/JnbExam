89f7e73feb3c95593fd6f555e813a98149c7f67b
edjia <jxh@live.com>
2021-09-19 14:58:44 +0800

    optimize jump

--- Jump.cpp
+++ Jump.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
-int main(){
-    vector<string> msg = {"hello", "world"};
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-
-    cout << endl;
+int main()
+{
+    int jump = 0, score = 0, cscore = 0;
     
+    while (cin >> jump)
+    {
+        if (jump == 0)
+        {
+            break;
+        }
+        if (jump == 1)
+        {
+            score++;
+            cscore = 0;
+        }
+        if (jump == 2)
+        {
+            cscore += 2;
+            score += cscore;
+        }
+    }
+    cout << score;
 }
