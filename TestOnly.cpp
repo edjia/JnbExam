324d52da5d0d0481a633272f9c3c943af3ae4964
edjia <jxh@live.com>
2021-09-19 13:43:07 +0800

    optimize sell vegetables

---
+++ TestOnly.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int jump = 0, score = 0, cscore = 0;
+    
+    while (1)
+    {
+        cin >> jump;
+        if (jump == 0)
+        {
+            break;
+        }
+        if (jump == 1)
+        {
+            score++;
+            cscore = 0;
+        }
+        if (jump == 2)
+        {
+            cscore += 2;
+            score += cscore;
+        }
+    }
+    cout << score;
+}
