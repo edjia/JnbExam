4c83da55002516a1aa705f24441b0f3a6d0fb3c4
edjia <jxh@live.com>
2022-06-10 15:05:11 +0800

    Frog

---
+++ StageExercise01/ForgJumpOverRiver.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int riverLen = 10;
+    int jumpMin = 2, jumpMax = 3;
+    int stoneCount = 5;
+    int stonePos[stoneCount] = {2, 3, 5, 6, 7};
+
+    for (int i = 0; i < riverLen; i++)
+    {
+        
+    }
+    
+}
